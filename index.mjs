e23197fe9bb21fcc489bad6720c44dc8464711c0
Guru Coyote <gc@testbild.tv>
2022-03-11 15:36:19 +0100

    * start of actual interactive browsing, using the accessebilityTreeSnapshot

--- index.mjs
+++ index.mjs
@@ -1,36 +1,17 @@
 import { chromium } from "playwright-chromium";
-
 import Vorpal from "vorpal";
 import grep from "vorpal-grep";
 // https://github.com/sindresorhus/ansi-escapes
 import ansiEscapes from "ansi-escapes";
+
 const vorpal = Vorpal();
-let browser, page;
-async function setup() {
-  browser = await chromium.launch({
-    executablePath: process.env.CHROME_BIN,
-  });
-
-  page = await browser.newPage();
-  page.setViewportSize({ width: 600, height: 400 });
-
-  // await browser.close();
-}
-async function run() {
-  await setup();
-  const res = await page.goto("https://ifconfig.io/ip");
-  const body = await res.body();
-  console.log("body", body.toString());
-}
-async function goto(url) {
-  await setup(); // ;
-  const res = await page.goto(url);
-  const body = await res.body();
-  // console.log("body", body.toString());
-  return body.toString();
-}
-
 vorpal.use(grep);
+let browser, page, acts, currIndex, currEl;
+// console.log(process.argv);
+let url = process.argv[2];
+if (url) {
+  await goto(url);
+}
 
 vorpal
   .mode("repl")
@@ -40,21 +21,36 @@ vorpal
     try {
       this.log(eval(command));
     } catch (e) {
-      this.log(e);
+      this.log(e.message);
     }
     callback();
   });
-vorpal.command("clear", "clear the screen").action(function (args, callback) {
-  process.stdout.write(ansiEscapes.eraseScreen);
-  callback();
-});
-vorpal.command("goto <url>", "goto an url").action(function (args, callback) {
-  this.log("going to " + args.url);
-  goto(args.url).then((r) => {
-    this.log(r);
+vorpal
+  .command("clear", "clear the screen") //
+  .action(function (_, callback) {
+    process.stdout.write(ansiEscapes.eraseScreen);
     callback();
   });
-});
+vorpal
+  .command("goto <url>", "goto an url") //
+  .action(function (args, callback) {
+    this.log("going to " + args.url);
+    goto(args.url).then(async () => {
+      callback();
+    });
+  });
+vorpal
+  .command("next [n]", "next [n] elemen(s)t in accessebilty tree")
+  .action(function (args, callback) {
+    reportElements(null, args.n || 1);
+    callback();
+  });
+vorpal
+  .command("prev [n]", "previous [n] element(s) in accessebilty tree")
+  .action(function (args, callback) {
+    reportElements(currIndex - (args.n || 1), args.n || 1);
+    callback();
+  });
 vorpal
   .command("get <el>", "gets and element and prints its innertext")
   .action(function (args, cb) {
@@ -64,3 +60,56 @@ vorpal
     });
   });
 vorpal.delimiter("will>").show();
+
+async function reportElements(start, number) {
+  if (!acts) return;
+  if (!start) start = currIndex + 1;
+  acts.children.map((el, i) => {
+    if (i >= start && i < start + number) {
+      console.log(accFormatElement(el));
+      currIndex = i;
+      currEl = el;
+    }
+  });
+}
+function accFormatElement(el) {
+  let out = "";
+  switch (el.role) {
+    case "text":
+      // don't prefix text elements
+      break;
+    case "heading":
+      out = el.role + el.level;
+      break;
+    default:
+      out = el.role;
+      break;
+  }
+  out += ` : ${el.name}`;
+  return out;
+}
+async function setup() {
+  browser = await chromium.launch({
+    executablePath: process.env.CHROME_BIN,
+  });
+
+  page = await browser.newPage();
+  page.setViewportSize({ width: 600, height: 400 });
+  page.on("load", async function (p) {
+    acts = await page.accessibility.snapshot();
+    console.log(acts.name, " at ", p.url());
+    console.log("page with ", acts.children.length, " elements");
+    currIndex = 0;
+    currEl = acts[currIndex];
+    reportElements(currIndex, 2);
+  });
+}
+async function goto(url) {
+  await setup(); // ;
+  try {
+    await page.goto(url);
+  } catch (e) {
+    console.warn("goto error: ", e.message);
+    return;
+  }
+}
